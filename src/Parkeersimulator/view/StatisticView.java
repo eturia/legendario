cae5941e5dd4a8d2c963ff2e1fcb1ed1015e4a49
Dennis van der Zwan <dennisvanderzwan@MacBook-Pro-van-Dennis.local>
2018-01-31 19:11:16 +0100

    Remover text fields

--- src/Parkeersimulator/view/StatisticView.java
+++ src/Parkeersimulator/view/StatisticView.java
@@ -21,65 +21,38 @@ public class StatisticView extends View {
         freeSpotsLabel = new JLabel("Vrije plekken");
         add(freeSpotsLabel);
 
-        // Vrije plekken text field
-        freeSpots = new JTextField();
-        freeSpots.setText("0");
-        freeSpots.setEditable(false);
-        freeSpots.setBackground(Color.WHITE);
-        add(freeSpots);
-
         //Totaal label
         totalCarsLabel = new JLabel("Totaal auto's");
         add(totalCarsLabel);
 
-        //Totaal textfield
-        totalCars = new JTextField();
-        totalCars.setText("0");
-        totalCars.setEditable(false);
-        totalCars.setBackground(Color.WHITE);
-        add(totalCars);
 
         //Adhoc label
         adHocLabel = new JLabel("AdHoc auto's");
         add(adHocLabel);
 
-        //Adhoc textfield
-        adHoc = new JTextField();
-        adHoc.setText("0");
-        adHoc.setEditable(false);
-        adHoc.setBackground(Color.WHITE);
-        add(adHoc);
 
         //ParkingPass label
         parkingPassLabel = new JLabel("ParkingPass");
         add(parkingPassLabel);
 
-        //ParkingPass textfield
-        parkingPass = new JTextField();
-        parkingPass.setText("0");
-        parkingPass.setEditable(false);
-        parkingPass.setBackground(Color.WHITE);
-        add(parkingPass);
 
         //Carqueue label
         carQueueLabel = new JLabel("Auto's in queue");
         add(carQueueLabel);
 
-        //Carqueue textfield
-        carQueue = new JTextField();
-        carQueue.setText("0");
-        carQueue.setEditable(false);
-        carQueue.setBackground(Color.WHITE);
-        add(carQueue);
     }
 
     public void update(Model model) {
         ParkingModel parkingModel = (ParkingModel)model;
 
-        freeSpots.setText("" + parkingModel.getGarage().getNumberOfOpenSpots());
-        totalCars.setText("" + parkingModel.getGarage().getTotalCars());
-        adHoc.setText("" + parkingModel.getGarage().getAdhoc().getCount());
-        parkingPass.setText("" + parkingModel.getGarage().getPass().getCount());
+        int freeSpots = parkingModel.getGarage().getNumberOfOpenSpots();
+        int totalCars = parkingModel.getGarage().getTotalCars();
+        int adHoc = parkingModel.getGarage().getAdhoc().getCount();
+        int parkingPass = parkingModel.getGarage().getPass().getCount();
 
+        freeSpotsLabel.setText("Vrije plekken: "+ freeSpots);
+        totalCarsLabel.setText("Totaal auto's: "+totalCars);
+        adHocLabel.setText("Totaal auto's die betaalt hebben: "+adHoc);
+        parkingPassLabel.setText("Totaal auto's met een abbonement: " +parkingPass);
     }
 }
